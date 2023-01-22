2188df7e71edd45a202010acf48a4d0a8bc74666
DenisM <mdr2010@yandex.ru>
2023-01-22 22:52:23 +0500

    add Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число D: ");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число E: ");
+int e = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int[] array = {a, b, c, d, e};
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.Write($"Введено чисел больше 0: {sum}");
